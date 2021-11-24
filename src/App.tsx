8e53ee510a35d69bfbeb390f50fa26eb9eed4e9e
Erikken-Hione <68046024+Erikken-Hione@users.noreply.github.com>
2021-11-24 13:31:50 +0600

    env

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.scss';
 const App: FC = () => {
     const timer = 0;
 
+    console.log(process.env.REACT_APP_MODE);
+
     return (
         <div className="app">
             {timer}
