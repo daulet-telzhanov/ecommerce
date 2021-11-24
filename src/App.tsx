647dc42180f20d702ca0a8f213f502c91725caa7
Erikken-Hione <68046024+Erikken-Hione@users.noreply.github.com>
2021-11-24 14:03:37 +0600

    config

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import './App.scss';
 const App: FC = () => {
     const timer = 0;
 
-    console.log(process.env.REACT_APP_MODE);
-
     return (
         <div className="app">
             {timer}
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,6 +3,7 @@ declare namespace NodeJS {
     interface ProcessEnv {
         NODE_ENV: 'development' | 'production' | 'test'
         PUBLIC_URL: string
-        REACT_APP_MODE: 'default' | 'testing'
+        REACT_APP_MODE: 'default' | 'testing',
+        REACT_APP_PUBLIC_URL: string
     }
 }
